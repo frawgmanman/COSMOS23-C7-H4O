ed7c4420bbf9cac7b8f9607a385843122a25e384
frawgmanman <cynthiayxnliu@gmail.com>
2023-07-31 13:56:11 -0700

    do is annoying

--- Sensors/dissolvedOxygen.ino
+++ Sensors/dissolvedOxygen.ino
@@ -1,64 +1,62 @@
-// to use the Atlas gravity circuits with 
-// the gravity isolator board's pulse output 
-// uncomment line 8: #define USE_PULSE_OUT
-// you can use any pins instead of just the analog ones
-// but it must be recalibrated
-// note that the isolator's analog output also provides isolation
 
-// #define USE_PULSE_OUT
-
-#ifdef USE_PULSE_OUT
-  #include "do_iso_grav.h"       
-  Gravity_DO_Isolated DO = Gravity_DO_Isolated(27);         
+#include <Arduino.h>
+ 
+#define DO_PIN A1
+ 
+#define VREF 5000    //VREF (mv)
+#define ADC_RES 1024 //ADC Resolution
+ 
+//Single-point calibration Mode=0
+//Two-point calibration Mode=1
+#define TWO_POINT_CALIBRATION 1
+ 
+#define READ_TEMP (25) //Current water temperature ℃, Or temperature sensor function
+ 
+//Single point calibration needs to be filled CAL1_V and CAL1_T
+#define CAL1_V (131) //mv
+#define CAL1_T (25)   //℃
+//Two-point calibration needs to be filled CAL2_V and CAL2_T
+//CAL1 High temperature point, CAL2 Low temperature point
+#define CAL2_V (1300) //mv
+#define CAL2_T (15)   //℃
+ 
+const uint16_t DO_Table[41] = {
+    14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
+    11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
+    9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
+    7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410};
+ 
+uint8_t Temperaturet;
+uint16_t ADC_Raw;
+uint16_t ADC_Voltage;
+uint16_t DO;
+ 
+int16_t readDO(uint32_t voltage_mv, uint8_t temperature_c)
+{
+#if TWO_POINT_CALIBRATION == 00
+  uint16_t V_saturation = (uint32_t)CAL1_V + (uint32_t)35 * temperature_c - (uint32_t)CAL1_T * 35;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
 #else
-  #include "do_grav.h"
-  Gravity_DO DO = Gravity_DO(27);
+  uint16_t V_saturation = (int16_t)((int8_t)temperature_c - CAL2_T) * ((uint16_t)CAL1_V - CAL2_V) / ((uint8_t)CAL1_T - CAL2_T) + CAL2_V;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
 #endif
-
-uint8_t user_bytes_received = 0;
-const uint8_t bufferlen = 32;
-char user_data[bufferlen];
-
-
-void parse_cmd(char* string) {
-  strupr(string);
-  String cmd = String(string);
-  if(cmd.startsWith("CAL")){
-    int index = cmd.indexOf(',');
-    if(index != -1){
-      String param = cmd.substring(index+1, cmd.length());
-      if(param.equals("CLEAR")){
-        DO.cal_clear();
-        Serial.println("CALIBRATION CLEARED");
-      }
-    }
-    else{
-      DO.cal();
-      Serial.println("DO CALIBRATED");
-    }
-  }
 }
-
-void setup() {
-  Serial.begin(9600);
-  delay(200);
-  Serial.println(F("Use command \"CAL\" to calibrate the circuit to 100% saturation in air\n\"CAL,CLEAR\" clears the calibration"));
-  if(DO.begin()){
-    Serial.println("Loaded EEPROM");
-  }
+ 
+void setup()
+{
+  Serial.begin(115200);
 }
-
-void loop() {
-  if (Serial.available() > 0) {
-    user_bytes_received = Serial.readBytesUntil(13, user_data, sizeof(user_data));
-  }
-
-  if (user_bytes_received) {
-    parse_cmd(user_data);
-    user_bytes_received = 0;
-    memset(user_data, 0, sizeof(user_data));
-  }
-  
-  Serial.println(DO.read_do_percentage());
+ 
+void loop()
+{
+  Temperaturet = (uint8_t)READ_TEMP;
+  ADC_Raw = analogRead(DO_PIN);
+  ADC_Voltage = uint32_t(VREF) * ADC_Raw / ADC_RES;
+ 
+  Serial.print("Temperaturet:\t" + String(Temperaturet) + "\t");
+  Serial.print("ADC RAW:\t" + String(ADC_Raw) + "\t");
+  Serial.print("ADC Voltage:\t" + String(ADC_Voltage) + "\t");
+  Serial.println("DO:\t" + String(readDO(ADC_Voltage, Temperaturet)) + "\t");
+ 
   delay(1000);
 }
